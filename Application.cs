c57b12dafccd52a667e710fd1a2d7da48a420798
blobSama <66427163+blobSama@users.noreply.github.com>
2022-05-08 18:45:49 +0300

    web service beginning

---
+++ Application.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace compuSciProj2021
+{
+    public class Application
+    {
+        private string userId;
+        private string workId;
+        private string workplaceReply;
+        private string cv;
+
+        public Application()
+        {
+
+        }
+
+        public string UserId
+        {
+            get
+            {
+                return this.userId;
+            }
+            set
+            {
+                this.userId = value;
+            }
+        }
+
+        public string WorkId
+        {
+            get
+            {
+                return this.workId;
+            }
+            set
+            {
+                this.workId = value;
+            }
+        }
+
+        public string Reply
+        {
+            get
+            {
+                return this.workplaceReply;
+            }
+            set
+            {
+                this.workplaceReply = value;
+            }
+        }
+
+        public string Cv
+        {
+            get
+            {
+                return this.cv;
+            }
+            set
+            {
+                this.cv = value;
+            }
+        }
+    }
+}
